9899141053a422b479d872dc81042dcd57b2623d
pavani <pavaniavni456@gmail.com>
2022-02-09 11:43:00 +0530

    added studentclasslist

---
+++ StudentClassList/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace StudentClassList
+{
+    public class StudentClass
+    {
+        static void Main(string[] args)
+        {
+            
+            List<String> SIds = new List<String>();
+            
+            List<string> SNames = new List<string>();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter Student ID : ");
+                SIds.Add(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.WriteLine("Enter Student Name : ");
+                SNames.Add(Console.ReadLine());
+                Console.WriteLine();
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The Student Id is : ");
+            foreach (var s in SIds)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The Student Names are :");
+            foreach (var s1 in SNames)
+            {
+                Console.WriteLine(s1);
+
+            }
+
+            
+        }
+    }
+}
